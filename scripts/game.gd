48a62972a4e54338cea960ea408ae22151e4769b
camilabocangel <bocangelcamila@gmail.com>
2025-09-16 16:25:05 -0400

    nada

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,3 +4,6 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	player.death.connect(_on_game_over)
+	
+func _on_game_over():
+	get_tree().change_scene_to_file("res://scenes/game_over.tscn")
