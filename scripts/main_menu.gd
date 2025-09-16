31f101d42bf086698b9d5b364f6beac009f30bcf
cosdealpha <bocangelcamila@gmail.com>
2025-09-16 15:52:09 -0400

    Menu activado

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,16 +1,12 @@
 extends Node2D
 
 @onready var play_button = $Play_Button
-@onready var map = $TileMap
-
-func _ready() -> void:
-	map.set_script(null)
 
 func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_texture_button_mouse_entered() -> void:
-	play_button.scale = Vector2(0.3, 0.3)
+	play_button.scale = Vector2(0.9, 0.9)
 
 func _on_texture_button_mouse_exited() -> void:
-	play_button.scale = Vector2(0.2, 0.2)
+	play_button.scale = Vector2(0.8, 0.8)
