60ca44c7b3799902267e92a6f65c2b48d3aa5a6f
camilabocangel <bocangelcamila@gmail.com>
2025-09-16 16:11:37 -0400

    Pequeño detalle de los botones

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -7,16 +7,20 @@ func _on_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func _on_menu_button_mouse_entered() -> void:
-	menu_button.scale = Vector2(0.3, 0.3)
+	menu_button.scale = Vector2(0.9, 0.9)
+	menu_button.modulate = Color(1.2, 1, 0, 1)
 
 func _on_menu_button_mouse_exited() -> void:
-	menu_button.scale = Vector2(0.2, 0.2)
+	menu_button.scale = Vector2(0.8, 0.8)
+	menu_button.modulate = Color(1, 1, 1, 1)
 
 func _on_replay_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_replay_button_mouse_entered() -> void:
-	replay_button.scale = Vector2(0.3, 0.3)
+	replay_button.scale = Vector2(0.9, 0.9)
+	replay_button.modulate = Color(1.2, 1, 0, 1)
 
 func _on_replay_button_mouse_exited() -> void:
-	replay_button.scale = Vector2(0.2, 0.2)
+	replay_button.scale = Vector2(0.8, 0.8)
+	replay_button.modulate = Color(1, 1, 1, 1)
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -7,6 +7,8 @@ func _on_texture_button_pressed() -> void:
 
 func _on_texture_button_mouse_entered() -> void:
 	play_button.scale = Vector2(0.9, 0.9)
+	play_button.modulate = Color(1.2, 1, 0, 1)
 
 func _on_texture_button_mouse_exited() -> void:
 	play_button.scale = Vector2(0.8, 0.8)
+	play_button.modulate = Color(1, 1, 1, 1)
