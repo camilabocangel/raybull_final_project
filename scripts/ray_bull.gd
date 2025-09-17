e104c2575e016ee0da8b0b0cd6ff65d2ed41ccbe
Santiago Acha <santiagoacha10@gmail.com>
2025-09-17 13:41:10 -0400

    RayStart

--- scripts/ray_bull.gd
+++ scripts/ray_bull.gd
@@ -1,25 +1,71 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 20
+const MAX_SPEED = 80
+const ACCEL = 100
+@export var hp = 50
+var direction = 1
+@export var target = Vector2.ZERO
+var player_detected = false
 
+signal deathSignal
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var hitbox: Area2D = $HitBox
+@onready var collision_shape_left: CollisionShape2D = $HitBox/CollisionShapeLeft
+@onready var collision_shape_right: CollisionShape2D = $HitBox/CollisionShapeRight
+@onready var detection_area: Area2D = $DetectionArea 
+
+
+
+func _ready() -> void:
+	target = global_position
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	if not animated_sprite:
+		return
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	velocity.x = 0
+	
+	#player detected - follow player
+	if player_detected:
+		var overlaps = detection_area.get_overlapping_areas()
+		if overlaps.size() > 0:
+			var player = overlaps[0]
+			target = player.global_position
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		var directions = global_position.direction_to(target).normalized()
+		velocity.x = directions.x * MAX_SPEED
+		
+		if directions.x > 0:
+			animated_sprite.flip_h = false
+			direction = 1 
+		elif directions.x < 0:
+			animated_sprite.flip_h = true
+			direction = -1 
+	
 	move_and_slide()
+	
+func _on_hurt_box_area_entered(area: Area2D) -> void:
+	hp -= 10
+	print("OUCHHHHHHHH (Bull)")
+	if hp <= 0 and animated_sprite.animation != "death":
+		animated_sprite.play("death")
+		deathSignal.emit()
+
+func _on_detection_area_area_entered(area: Area2D) -> void:
+	player_detected = true 
+	animated_sprite.play("attack")
+
+func _on_detection_area_area_exited(area: Area2D) -> void:
+	player_detected = false 
+	target = global_position
+	animated_sprite.play("run")
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	pass # Replace with function body.
